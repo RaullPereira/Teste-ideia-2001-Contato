0047416e74838e47282357a2f3137b8f7c186153
Raul <110264497+Raulpss@users.noreply.github.com>
2022-10-18 06:11:04 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,15 @@
+//VOLTAR PAGINA//
+
+var button = document.querySelector(".icon-voltar");
+
+button.onclick = function() {
+    window.location.href = "https://raullpereira.github.io/Teste-ideia-2001-Home/"
+}
+
+//VOLTAR PAGINA PELA LOGO//
+
+var button = document.querySelector(".logo");
+
+button.onclick = function() {
+    window.location.href = "https://raullpereira.github.io/Teste-ideia-2001-Home/"
+}
